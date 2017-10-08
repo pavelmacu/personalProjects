bf014ec8936bd5d61bc6cb41c84c60497963014e
pavelmacu <pavel.alfredo.macutela@estudiant.upc.edu>
2017-10-08 18:57:49 +0200

    borrando xxxx

--- twit/holatwitter.py
+++ twit/holatwitter.py
@@ -1,10 +1,10 @@
 import tweepy, sys
 
 # Create variables for each key, secret, token
-consumer_key = 'vVCZ3YAYempvjdWMTh2nQAqyl'
-consumer_secret = 'XPhTonpHBxKSXNdf1SSJgqYWmZdEHKbGTjGG5WlyZZjCTnovsZ'
-access_token = '2180284776-wlDi1ZYlpdZw7RpenDdL7BmS3rnTgbR8q1OHw7N'
-access_token_secret = 'Q0Mo7gosc6xzDFQov0YxL3nJ2OF0BOdhq4gaYE2PQ3ONP'
+consumer_key = 'xxxxxxxxxxxxxxxxx'
+consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # Set up OAuth and integrate with API
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
